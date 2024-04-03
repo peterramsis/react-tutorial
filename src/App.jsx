d20aca7ef7011579808c69609ad095b2a77421fb
peterramsis <peterzizo100@gmail.com>
2024-04-03 09:19:29 +0200

    Add sass package and styles for navbar
    Added the "sass" package version 1.72.0 and included styles for the navbar
    component in the main.scss file.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import Navbar from "./components/navbar";
+import './sass/main.scss';
 const app = () => {
   return (
     <div>
       <Navbar></Navbar>
-
       <section className="container  mx-auto">
         <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
           <div>col 1</div>
-
           <div>col 1</div>
           <div>col 1</div>
           <div>col 1</div>
